f2c56853c005f8de01472e9bd975bb6dded9968d
Ponleou Keo Sok <ponleousk@gmail.com>
2024-04-29 21:22:37 +0700

    feat: build the layers for model

--- models/facial-expression-cv/facial-expression-cv.py
+++ models/facial-expression-cv/facial-expression-cv.py
@@ -1,8 +1,10 @@
 import cv2
-import tensorflow as tf
 import os
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow.keras import layers
 
 # execute inside the folder
 datasets_dir = "../../datasets/"
@@ -13,7 +15,7 @@ fe_dataset_dir = os.path.join(datasets_dir, "facial-expression-dataset")
 # img = cv2.imread(os.path)
 
 # there should be 2400 different images for each lable/category
-max_data_per_label = 2400
+max_data_per_label = 500
 img_size = 224
 
 fe_dataset = []
@@ -24,28 +26,60 @@ for label in labels:
     i = 0
     for img_file in os.listdir(label_dir):
         img_path = os.path.join(label_dir, img_file)
-        img_array = cv2.imread(img_path)
-        img_resize_array = cv2.resize(img_array, (img_size, img_size))
+        img_array = cv2.imread(
+            img_path
+        )  # converting images to multi dimension array with opencv
+        img_array_rgb = cv2.cvtColor(
+            img_array, cv2.COLOR_BGR2RGB
+        )  # changing color from bgr to rgb (default is bgr)
+        img_resize_array = cv2.resize(
+            img_array_rgb, (img_size, img_size)
+        )  # changing the image resolution from 96 to 224, because our pretrain-model inputs (224,224,3) shape
         fe_dataset.append([img_resize_array, label_index])
         i += 1
         if i == max_data_per_label:
             break
 
-# print(len(fe_dataset))
-# print(fe_dataset[23][0].shape) # (224, 224, 3) cus rgb
-
-# print(np.array(fe_dataset))
-# print(fe_dataset.shape)
-
 feature_ds = []
 label_ds = []
 for feature, label in fe_dataset:
-    feature_ds.append(feature)
+    feature_ds.append(feature / 255)  # normalizing the data
     label_ds.append(label)
 
-# print(np.array(fe_dataset))
-# print(np.array(fe_dataset))
+batch_size = 16
 
-print(np.array(feature_ds))
-print(np.array(label_ds))
-print(len(feature_ds), len(label_ds))
+raw_ds = (
+    tf.data.Dataset.from_tensor_slices((np.array(feature_ds), np.array(label_ds)))
+    .batch(batch_size)
+    .shuffle(1000)
+)
+
+train_size = int(0.64 * len(raw_ds))
+val_size = int(0.16 * len(raw_ds))
+test_size = int(0.2 * len(raw_ds))
+
+train_ds = raw_ds.take(train_size)
+test_ds = raw_ds.skip(train_size)
+val_ds = raw_ds.skip(test_size)
+test_ds = raw_ds.take(test_size)
+
+pretrained_model = tf.keras.applications.MobileNetV2()
+
+pretrained_model_output = pretrained_model.layers[-2].output
+pretrained_model_output = layers.Dense(128, activation="relu")(pretrained_model_output)
+pretrained_model_output = layers.Dense(64, activation="relu")(pretrained_model_output)
+prediction_output = layers.Dense(8, activation="softmax")(pretrained_model_output)
+
+model = tf.keras.Model(
+    inputs=pretrained_model.layers[0].input, outputs=prediction_output
+)
+
+model.compile(
+    loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+)
+
+epochs = 10
+model.fit(train_ds, epochs=epochs, validation_data=val_ds)
+
+loss, acc = model.evaluate(test_ds)
+print("Loss: ", loss, "Accuracy: ", acc)
